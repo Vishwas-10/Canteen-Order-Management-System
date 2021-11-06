15652b938786b8fbb1e9d1f7b7677359d89ddcf5
Vishwas-10 <79042063+Vishwas-10@users.noreply.github.com>
2021-11-06 22:25:28 +0530

    updated app.js
    added jump links to css grid and hostel no to sign up page

--- final_project/app.js
+++ final_project/app.js
@@ -56,3 +56,24 @@ $(document).ready(function(){
         }
     });
 });
+
+// offsetting the # link in anchor (css grid button)
+
+// The function actually applying the offset
+function offsetAnchor() {
+    if (location.hash.length !== 0) {
+      window.scrollTo(window.scrollX, window.scrollY - 100);
+    }
+  }
+  
+  // Captures click events of all <a> elements with href starting with #
+  $(document).on('click', 'a[href^="#"]', function(event) {
+    // Click events are captured before hashchanges. Timeout
+    // causes offsetAnchor to be called after the page jump.
+    window.setTimeout(function() {
+      offsetAnchor();
+    }, 0);
+  });
+  
+  // Set the offset when entering page with hash present in the url
+  window.setTimeout(offsetAnchor, 0);
