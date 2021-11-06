b962812fc2a8fce6d616b35ed2b211f37e3f2b48
Vishwas-10 <79042063+Vishwas-10@users.noreply.github.com>
2021-11-06 09:48:35 +0530

    Updated app.js
    Updated app.js

--- final_project/app.js
+++ final_project/app.js
@@ -3,7 +3,6 @@ $(window).scroll(function(){
     $('nav').toggleClass('scrolled', $(this).scrollTop()>650);
 });
 
-
 var login = document.getElementById("upperLogin");
 
 function openLogin(){
@@ -19,3 +18,25 @@ window.onclick = function(event) {
         login.style.display = "none";
     }
 }
+
+$(document).ready(function(){
+
+
+    // top sale owl carousel
+    $(".Food-Carousel .owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        dots: false,
+        responsive : {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000 : {
+                items: 5
+            }
+        }
+    });
+});
